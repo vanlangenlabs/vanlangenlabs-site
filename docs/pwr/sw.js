3d44dc252ed89d4436eea52709089799ae0d28a2
Jeroen <jeroen@live.nl>
2026-09-23 16:16:11 +0200

    added some missing files

---
+++ docs/pwr/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "pwr-contact-v1";
+
+const FILES = [
+  "/pwr/",
+  "/pwr/pwrpack.vcf",
+  "/pwr/vcard-url.png",
+  "/pwr/vcard.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key.startsWith("pwr-contact-") && key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
